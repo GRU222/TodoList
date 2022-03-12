f9e60c1d4e8b99f9481033a0e36992684eff8b7b
buyantogtoh-y1 <buyantogtoh-y1@iMac-17s-iMac.local>
2022-03-12 11:17:51 +0800

    issue0

---
+++ public/todoList.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1bmOY6K1At5IXTEwHDzWvf4-X78QJrmk",
+    authDomain: "todolist-eb137.firebaseapp.com",
+    projectId: "todolist-eb137",
+    databaseURL:"https://todolist-eb137-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    storageBucket: "todolist-eb137.appspot.com",
+    messagingSenderId: "788653159519",
+    appId: "1:788653159519:web:481a2f3cd4774c0782d7ea"
+};
+const app = initializeApp(firebaseConfig);
